4a38b04bb9dacb04f9c7955bcb158ff558ccadf3
Jason Vu <jvu@scu.edu>
2025-07-09 14:00:41 -0700

    replaced smtp with http requests

--- commands/practicality/verify.js
+++ commands/practicality/verify.js
@@ -1,13 +1,10 @@
-const {
-    Command
-} = require("discord.js-commando");
-const nodemailer = require("nodemailer");
+const { Command } = require("discord.js-commando");
 const _ = require("lodash");
 const dot = require("dot");
 const fs = require("fs-extra");
-const uniqueID = new Set();
+const { Resend } = require("resend");
+const uniqueID = new Set(); 
 
-// Metadata
 module.exports = class verifyCommand extends Command {
     constructor(client) {
         super(client, {
@@ -19,107 +16,97 @@ module.exports = class verifyCommand extends Command {
                 usages: 2,
                 duration: 5,
             },
-            args: [{
-                key: "emailAddress",
-                prompt: "Please enter your SCU email!",
-                type: "string",
-                oneOf: ["bbronco@scu.edu"],
-                validate: (emailAddress) => {
-                    if (/\w+@scu.edu$/.test(emailAddress) || /\w+@alumni.scu.edu/.test(emailAddress)) { // Correct email
-                        return true;
-                    } else {
-                        return "Please enter a valid email address!";
+            args: [
+                {
+                    key: "emailAddress",
+                    prompt: "Please enter your SCU email!",
+                    type: "string",
+                    oneOf: ["bbronco@scu.edu"],
+                    validate: (emailAddress) => {
+                        if (/\w+@scu.edu$/.test(emailAddress) || /\w+@alumni.scu.edu/.test(emailAddress)) {
+                            return true;
+                        } else {
+                            return "Please enter a valid email address!";
+                        }
                     }
+                },
+                {
+                    key: "roleOptions",
+                    prompt: "Please choose a valid option: `student or scu faculty/staff`",
+                    type: "string",
+                    oneOf: ["student", "scu faculty/staff"],
                 }
-            }, 
-            {
-                key: "roleOptions",
-                prompt: "Please choose a valid option: \`student or scu faculty/staff\`",
-                type: "string",
-                oneOf: ["student", "scu faculty/staff"],
-            },]
+            ]
         });
     }
 
     async run(message, { emailAddress, roleOptions }) {
         message.delete();
 
-        if (message.member.roles.cache.some((role) => role.name === "student") || message.member.roles.cache.some((role) => role.name === "scu faculty/staff")) {
+        if (message.member.roles.cache.some(role => role.name === "student") ||
+            message.member.roles.cache.some(role => role.name === "scu faculty/staff")) {
             return this.client.error("This user is already verified!", message);
         }
 
         if (message.channel.id !== this.client.config.channels.access) {
             return this.client.error(`This can only be used in the <#${this.client.config.channels.access}> channel!`, message);
-        } 
+        }
 
         if (!uniqueID.has(message.author.id)) {
             uniqueID.add(message.author.id);
         }
 
-        const transporter = nodemailer.createTransport({
-            host: this.client.config.email.domain,
-            port: 465,
-            secure: true, // use SSL
-            auth: {
-                user: this.client.config.email.username,
-                pass: this.client.config.email.password,
-            },
-        });
+        const resend = new Resend(this.client.config.resend_api_key); 
 
         const rawHTML = await fs.readFile("./assets/templateFiles/email.html", "utf8");
         const tempFn = dot.template(rawHTML);
         const keycode = [0, 0, 0, 0].map(() => _.random(0, 9)).join("");
-        const result = tempFn({
-            keycode
-        }); 
+        const result = tempFn({ keycode });
 
-        const mailOptions = {
-            from: `${message.guild.name} <${this.client.config.email.username}>`,
-            to: `${emailAddress}`,
-            subject: `${message.guild.name} Verification`,
-            html: result,
-            text: `Thank you for verifying yourself in the server!\nVerification Code: ${keycode}\nTo verify yourself as a student, please respond back to the bot in a private message with the verification code above. This step will only be available for a short time. If you fail to verify your account, you will need to restart this process.\nIf you are not the intended recipient of this email, please contact ${this.client.config.email.username}.`,
-        };
+        try {
+            await resend.emails.send({
+                from: `SCU Discord Network <onboarding@resend.dev>`,
+                to: emailAddress,
+                subject: `${message.guild.name} Verification`,
+                html: result,
+                text: `Thank you for verifying yourself in the server!\nVerification Code: ${keycode}\nRespond in the <#${this.client.config.channels.access}> channel within 5 minutes.`,
+            });
 
-        await transporter.sendMail(mailOptions, async (error) => {
-            if (error) {
-                return this.client.error("Error occurred while sending email. Please redo the command.", error.message);
-            } else {
-                await message.author.send({
-                    embed: {
-                        title: "Email sent successfully!",
-                        description: `Email sent to **${emailAddress}**. Check your school email for your verification code, then type the keycode in the <#${this.client.config.channels.access}> channel in less than 5 minutes!`,
-                        color: this.client.config.school_color
-                    }
-                });
-                const keycodeFilter = (m) => { // Give the user some feedback
-                    if (m.author.bot) { // Don't listen to bot messages
-                        return false;
-                    }
-                    if (m.toString() === keycode) { // Correct keycode
-                        return true;
-                    }
-                    this.client.error("Invalid keycode. Please try again.", m); // Incorrect keycode
-                    return false;
-                };
+            await message.author.send({
+                embed: {
+                    title: "Email sent successfully!",
+                    description: `Email sent to **${emailAddress}**. Check your school email for the verification code and enter it in the <#${this.client.config.channels.access}> channel within 5 minutes!`,
+                    color: this.client.config.school_color
+                }
+            });
 
-                await message.channel.awaitMessages(keycodeFilter, {
-                    max: 1,
-                    time: 300000,
-                    errors: ["time"]
-                }).then((collected) => collected.first().delete()); // exactly 5 minutes
+            const keycodeFilter = (m) => {
+                if (m.author.bot) return false;
+                if (m.toString() === keycode) return true;
+                this.client.error("Invalid keycode. Please try again.", m);
+                return false;
+            };
 
-                let role = message.guild.roles.cache.find((role) => role.name === roleOptions);
-                await message.member.roles.add(role).then(() => {
-                    message.author.send(`<@${message.author.id}>`, { embed: {
-                            title: "Welcome to the Santa Clara University Discord Network!",
-                            description: `You have been verified! You can now access the rest of the server and select your roles in the <#${this.client.config.channels.roles}> channel!`,
-                            color: this.client.config.school_color
-                    }});
-                });
-            }
-        });
-        
-        uniqueID.delete(message.author.id); // Remove the user from the set
+            await message.channel.awaitMessages(keycodeFilter, {
+                max: 1,
+                time: 300000,
+                errors: ["time"]
+            }).then(collected => collected.first().delete());
+
+            let role = message.guild.roles.cache.find(role => role.name === roleOptions);
+            await message.member.roles.add(role);
+
+            await message.author.send(`<@${message.author.id}>`, {
+                embed: {
+                    title: "Welcome to the Santa Clara University Discord Network!",
+                    description: `You have been verified! You can now access the rest of the server and select your roles in the <#${this.client.config.channels.roles}> channel!`,
+                    color: this.client.config.school_color
+                }
+            });
+        } catch (error) {
+            return this.client.error("Error occurred while sending email. Please redo the command.", message);
+        }
+
+        uniqueID.delete(message.author.id);
     }
-};
+}; 
